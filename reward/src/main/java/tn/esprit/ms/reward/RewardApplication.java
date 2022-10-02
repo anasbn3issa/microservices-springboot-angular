7677bd95e05f12234059327d8efaa6d3c3783733
anas <anasben3issa@gmail.com>
2022-10-03 00:35:57 +0100

    created entity, controller and repository post request functional delete request functional get All request functional

--- reward/src/main/java/tn/esprit/ms/reward/RewardApplication.java
+++ reward/src/main/java/tn/esprit/ms/reward/RewardApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RewardApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RewardApplication.class, args);
 	}
