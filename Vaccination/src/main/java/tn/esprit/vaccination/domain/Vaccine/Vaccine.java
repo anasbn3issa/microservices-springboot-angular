0c35874db5e81082bfefae903e8a709b59161567
omar karboul <karboulomar@gmail.com>
2022-10-13 18:16:12 +0100

    added docker file and fixed relations

--- Vaccination/src/main/java/tn/esprit/vaccination/domain/Vaccine/Vaccine.java
+++ Vaccination/src/main/java/tn/esprit/vaccination/domain/Vaccine/Vaccine.java
@@ -1,9 +1,12 @@
 package tn.esprit.vaccination.domain.Vaccine;
 
 import lombok.Data;
+import tn.esprit.vaccination.domain.Pet;
 import tn.esprit.vaccination.domain.VaccineType.VaccineType;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "vaccines")
@@ -16,15 +19,21 @@ public class Vaccine {
     private Boolean validity;
     private int quantity;
 
+    @OneToMany(mappedBy = "vaccine",cascade = CascadeType.REMOVE)
+    private List<Pet> pets;
+
+
     @ManyToOne
     @JoinColumn(name = "vaccine_type_id")
     private VaccineType vaccineType;
 
-    public Vaccine(int id, String name, Boolean validity, int quantity, VaccineType vaccineType) {
+
+    public Vaccine(int id, String name, Boolean validity, int quantity, List<Pet> pets, VaccineType vaccineType) {
         this.id = id;
         this.name = name;
         this.validity = validity;
         this.quantity = quantity;
+        this.pets = pets;
         this.vaccineType = vaccineType;
     }
 
