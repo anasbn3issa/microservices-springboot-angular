b1dc7e380c6e133c9d576dd7e9b66d3dea10fa83
geeekho <wajdi.sadouki@gmail.com>
2022-10-18 23:07:00 +0100

    fixed docker compose eureka error

--- sterilization-veterinarian/src/main/java/com/esprit/microservice/configuration/RedisConfig.java
+++ sterilization-veterinarian/src/main/java/com/esprit/microservice/configuration/RedisConfig.java
@@ -15,24 +15,24 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @EnableRedisRepositories
 public class RedisConfig {
     
-    @Value("${spring.redis.host}")
+    @Value("${redis.host}")
     private String REDIS_HOST;
-    @Value("${spring.redis.port}")
+    @Value("${redis.port}")
     private Integer REDIS_PORT;
 
-	@Bean
-	  public RedisConnectionFactory connectionFactory() {
-        
-	    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("redis", REDIS_PORT);
-	    return new JedisConnectionFactory(config);
-	  }
+    @Bean 
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConFactory = new  JedisConnectionFactory();
+     jedisConFactory.setHostName(REDIS_HOST);
+     jedisConFactory.setPort(REDIS_PORT); 
+     return jedisConFactory; 
+    }
 
-	  @Bean
-	  public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-
-	    RedisTemplate<byte[], byte[]> template = new RedisTemplate<byte[], byte[]>();
-	    template.setConnectionFactory(redisConnectionFactory);
-	    return template;
-	  }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
 
 }
