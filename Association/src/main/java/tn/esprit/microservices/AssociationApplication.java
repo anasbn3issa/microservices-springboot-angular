41b5898b237a303d4bf83f6b72e673d1ded3b866
Cyrine Louati <cyrine.louati@esprit.tn>
2022-10-21 21:40:47 +0100

    updated pom.xml + application+properties V2.2

--- Association/src/main/java/tn/esprit/microservices/AssociationApplication.java
+++ Association/src/main/java/tn/esprit/microservices/AssociationApplication.java
@@ -5,14 +5,15 @@ import java.util.stream.Stream;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-
+@EnableEurekaClient
 @SpringBootApplication
 public class AssociationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssociationApplication.class, args);
-		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+		System.out.println("XXXXXXX Testing XXXXXXX");
 	}
 
 
