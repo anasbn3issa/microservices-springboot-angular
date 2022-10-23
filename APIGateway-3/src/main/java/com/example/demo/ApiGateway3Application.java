24357ed9e63335b29cb33f8a64f3e9eb5979d4b8
omar karboul <karboulomar@gmail.com>
2022-10-23 12:57:59 +0100

    added gateway

---
+++ APIGateway-3/src/main/java/com/example/demo/ApiGateway3Application.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGateway3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateway3Application.class, args);
+	}
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+
+		return builder.routes()
+
+				.route("vaccination", r -> r.path("/vaccination/**")
+						.uri("http://localhost:8091/"))
+				.build();
+
+	}
+
+}
