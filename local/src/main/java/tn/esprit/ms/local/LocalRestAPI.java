5f1a4f6178b93eb2ef14066238ffda1649e4bc29
Bilel Taktak <bilel.taktak@esprit.tn>
2022-10-23 16:57:38 +0200

    configure docker so it runs local image

--- local/src/main/java/tn/esprit/ms/local/LocalRestAPI.java
+++ local/src/main/java/tn/esprit/ms/local/LocalRestAPI.java
@@ -1,36 +1,65 @@
 package tn.esprit.ms.local;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.MediaType;
 
 @RestController
-@RequestMapping("/locals")
+@RequestMapping(value = "/api/locals")
 public class LocalRestAPI {
+
     @Autowired
-    LocalRepository localRepository;
+    private LocalService localService;
 
-    @RequestMapping("/all")
-    public Iterable<Local> getAllLocals() {
-        return localRepository.findAll();
+    // Path : http://localhost:8081/api/locals
+    @GetMapping
+    @ResponseBody
+    public List<Local> getLocals() {
+        List<Local> listlocals = localService.retrieveLocals();
+        return listlocals;
     }
 
-    @RequestMapping("/add")
-    public Local addLocal(@RequestBody Local local) {
-        return localRepository.save(local);
+    // Path : http://localhost:8081/api/locals/:id
+    @GetMapping("/{id}")
+    @ResponseBody
+    public Local getLocal(@PathVariable("id") int id) {
+        Local local = localService.retrieveLocal(id);
+        return local;
     }
 
-    @RequestMapping("/delete/{id}")
-    public void deleteLocal(@PathVariable int id) {
-        localRepository.deleteById(id);
+    // Path : http://localhost:8081/api/locals
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Local> createCandidat(@RequestBody Local local) {
+        return new ResponseEntity<>(localService.addLocal(local), HttpStatus.OK);
     }
 
-    @RequestMapping("/update/{id}")
-    public Local updateLocal(@PathVariable int id, @RequestBody Local local) {
-        local.setId(id);
-        return localRepository.save(local);
+    // Path : http://localhost:8081/api/locals/update/:id
+    @PutMapping("/update/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Local> updateLocal(@PathVariable(value = "id") int id,
+            @RequestBody Local candidat) {
+        return new ResponseEntity<>(localService.updateLocal(id, candidat), HttpStatus.OK);
+    }
+
+    // Path : http://localhost:8081/api/locals/delete/:id
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> deleteLocal(@PathVariable(value = "id") int id) {
+        return new ResponseEntity<>(localService.deleteLocal(id), HttpStatus.OK);
     }
 
 }
