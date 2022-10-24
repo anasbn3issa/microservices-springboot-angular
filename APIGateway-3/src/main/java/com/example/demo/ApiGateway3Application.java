9f928b5a0aed7d3d911663875be8f724b01b47d0
omar karboul <karboulomar@gmail.com>
2022-10-24 11:57:57 +0100

    added security to gateway

--- APIGateway-3/src/main/java/com/example/demo/ApiGateway3Application.java
+++ APIGateway-3/src/main/java/com/example/demo/ApiGateway3Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
+@EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
 public class ApiGateway3Application {
